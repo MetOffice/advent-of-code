d0f9f0462a5213a1cd8e2e97bca8102ac54b94fb
TomDufall <thomas.dufall@metoffice.gov.uk>
2020-03-25 15:17:27 +0000

    Add day 4 instruction, input, and template

---
+++ 2019/day_04/Python/team_template/load_input.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+from pathlib import Path
+
+Range = namedtuple("Range", ["min", "max"])
+
+def get_input() -> Range:
+    input_file = Path(__file__).resolve().parent.parent.parent / "input.txt"
+    with open(input_file, "r") as file:
+        input_str = file.readline()
+        _MIN, _MAX = input_str.split('-')
+
+    return Range(_MIN, _MAX)
---
+++ 2019/day_04/Python/team_template/part1.py
@@ -0,0 +1,17 @@
+from load_input import get_input
+
+
+def is_valid_password(password: str) -> bool:
+    # Note - the tests use this function name - update them if you rename this
+    return True
+
+
+def count_possible_passwords(min_: int, max_: int) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    input_ = get_input()
+    print(input_)
+    result = count_possible_passwords(input_.min, input_.max)
+    print(result)
