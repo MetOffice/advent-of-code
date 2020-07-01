66129e65b03c556935aa0c2619f699240ab22126
Emma Hogan <emma.hogan@metoffice.gov.uk>
2020-07-01 14:09:46 +0100

    Add class docstring, rename self.pointer to self.next_instruction_index, update tests to test output as well as resulting program

--- 2019/day_09/Python/refactor/computer.py
+++ 2019/day_09/Python/refactor/computer.py
@@ -5,10 +5,51 @@ from load_input import get_input
 
 
 class Computer:
+    """
+    The program list contains one or more instructions and corresponding
+    parameters. An instruction may contain one digit, e.g. 1 (a
+    one-digit opcode with the leading zero removed), three digits, e.g.
+    102 (a two-digit opcode and one parameter mode with two leading
+    zeros removed), or four digits, e.g. 1002 (a two-digit opcode and
+    two parameters modes with one leading zero removed):
+
+    ABCDE
+     1002
+
+    DE - two-digit opcode
+    C - mode of 1st parameter
+    B - mode of 2nd parameter
+    A - mode of 3rd parameter
+
+    where:
+
+    02 == opcode 2
+    0 == position mode
+    1 == immediate mode
+    0 == position mode, omitted due to being a leading zero
+
+    The mode can be either 0 or 1, where 0 refers to position mode,
+    which means the value of the item refers to the index in the program
+    list to be used, and 1 refers to immediate mode, which means the
+    value of the item in the program list is used directly.
+
+    ``parameter_modes`` is a list containing 3 integers corresponding
+    to the modes of the 1st, 2nd and 3rd parameter, respectively (which
+    differs from the description in the puzzles as described above, see
+    also the ``parse_opcode`` method).
+    """
     def __init__(self, program: List[int], input_buffer: List[int] = None):
+        """
+        Parameters
+        ----------
+        program: list of integers
+            The intcode program.
+        input_buffer: list of integers
+            The input.
+        """
         self.program = program
         self.running = True
-        self.pointer = 0
+        self.next_instruction_index = 0
         self.input_buffer = input_buffer
         if self.input_buffer is None:
             self.input_buffer = []
@@ -19,19 +60,19 @@ class Computer:
 
     # opcode 1
     def add(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
-        dest = self.get_address(parameter_modes[2], self.pointer + 3)
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
+        dest = self.get_address(parameter_modes[2], self.next_instruction_index + 3)
         self.program[dest] = value1 + value2
-        self.pointer += 4
+        self.next_instruction_index += 4
 
     # opcode 2
     def mult(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
-        dest = self.get_address(parameter_modes[2], self.pointer + 3)
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
+        dest = self.get_address(parameter_modes[2], self.next_instruction_index + 3)
         self.program[dest] = value1 * value2
-        self.pointer += 4
+        self.next_instruction_index += 4
 
     # opcode 3
     def get_and_store_input(self, parameter_modes):
@@ -39,61 +80,61 @@ class Computer:
         Get an input. Consume inputs from the input buffer before prompting
         with input().
         """
-        dest = self.get_address(parameter_modes[0], self.pointer + 1)
+        dest = self.get_address(parameter_modes[0], self.next_instruction_index + 1)
         if len(self.input_buffer) > 0:
             input_int = self.input_buffer.pop(0)
         else:
             self.wait()
             return
         self.program[dest] = input_int
-        self.pointer += 2
+        self.next_instruction_index += 2
 
     # opcode 4
     def output_int(self, parameter_modes):
-        value = self.get_address(parameter_modes[0], self.pointer + 1)
+        value = self.get_address(parameter_modes[0], self.next_instruction_index + 1)
         #print(self.program[value])
         self.last_output = self.program[value]
-        self.pointer += 2
+        self.next_instruction_index += 2
 
     # opcode 5
     def jump_if_true(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
         if value1 != 0:
-            self.pointer = value2
+            self.next_instruction_index = value2
         else:
-            self.pointer += 3
+            self.next_instruction_index += 3
 
     # opcode 6
     def jump_if_false(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
         if value1 == 0:
-            self.pointer = value2
+            self.next_instruction_index = value2
         else:
-            self.pointer += 3
+            self.next_instruction_index += 3
 
     # opcode 7
     def less_than(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
-        dest = self.get_address(parameter_modes[2], self.pointer + 3)
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
+        dest = self.get_address(parameter_modes[2], self.next_instruction_index + 3)
         if value1 < value2:
             self.program[dest] = 1
         else:
             self.program[dest] = 0
-        self.pointer += 4
+        self.next_instruction_index += 4
 
     # opcode 8
     def equals(self, parameter_modes):
-        value1 = self.program[self.get_address(parameter_modes[0], self.pointer + 1)]
-        value2 = self.program[self.get_address(parameter_modes[1], self.pointer + 2)]
-        dest = self.get_address(parameter_modes[2], self.pointer + 3)
+        value1 = self.program[self.get_address(parameter_modes[0], self.next_instruction_index + 1)]
+        value2 = self.program[self.get_address(parameter_modes[1], self.next_instruction_index + 2)]
+        dest = self.get_address(parameter_modes[2], self.next_instruction_index + 3)
         if value1 == value2:
             self.program[dest] = 1
         else:
             self.program[dest] = 0
-        self.pointer += 4
+        self.next_instruction_index += 4
 
     # opcode 99
     def die(self):
@@ -112,7 +153,7 @@ class Computer:
         return result
 
     def parse_opcode(self):
-        instruction = str(self.program[self.pointer])
+        instruction = str(self.program[self.next_instruction_index])
         opcode = int(instruction[-2:])
 
         parameter_modes = [0, 0, 0]
@@ -156,7 +197,7 @@ class Computer:
                 "Unrecognised opcode: "
                 + str(opcode)
                 + " at position "
-                + str(self.pointer)
+                + str(self.next_instruction_index)
             )
 
         return self.running
