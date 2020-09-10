ed3c0d4cfaa2dcb82222905b9ee917724896e5a6
esther.turner <esturner@vld306.cmpd1.metoffice.gov.uk>
2020-09-10 13:35:19 +0100

    Day 1 done

--- 2019/day_16/Python/template/part1.py
+++ 2019/day_16/Python/template/part1.py
@@ -1,107 +1,41 @@
 from load_input import get_input
-import math
-import pprint
-
-pp = pprint.PrettyPrinter()
+from itertools import cycle
 
 
-def parse_recipe(recipe_string):
-    """
+def calculate_digit(input_value, pattern):
+    result = 0
+    # Want to ignore the first digit in the pattern
+    pattern_cycle = cycle(pattern)
+    next(pattern_cycle)
+    for (input_digit, pattern_digit) in zip(input_value, pattern_cycle):
+        result += input_digit * pattern_digit
+    # Only want to return the last digit of the sum
+    result_string = str(result)[-1]
+    return int(result_string)
+        
 
-    """
-    parsed_recipe = {}
-    for component in recipe_string.split(', '):
-        quantity, chemical = component.split()
-        parsed_recipe[chemical] = int(quantity)
-    return parsed_recipe
-
-
-def parse_input(reactions):
-    """
-
-    """
-    parsed_input = {}
-    for line in reactions:
-        recipe, result = line.split(' => ')
-        quantity, chemical = result.split()
-        parsed_input[chemical] = {
-            'quantity': int(quantity),
-            'recipe': parse_recipe(recipe)
-        }
-    return parsed_input
+def calculate_phase(input_value):
+    result = []
+    base_pattern = [0, 1, 0, -1]
+    for index in range(1, len(input_value)+1):
+        pattern = perturb_pattern(base_pattern, index)
+        result.append(calculate_digit(input_value, pattern))
+    return result
 
 
-def walk_into():
-    """
-
-    """
-    pass
-
-
-def calculate_total_ore(reactions):
-    """
-    At the moment we are throwing away the quantity produced information
-    Idea is to make a copy of reactions. Divide value provided by quantity key
-    through the quantities in the value of the recipe key
-    """
-    pp.pprint(reactions)
-    ore = {}
-    fuel_details = reactions['FUEL']
-    ore = calculate_ore(fuel_details, reactions, ore)
-    total_ore = 0
-    for base_chemical, base_quantity in ore.items():
-        print("Base Chemical:", base_chemical, "Base Quantity", base_quantity)
-        minimum_quantity = reactions[base_chemical]["quantity"]
-        minimum_ore = reactions[base_chemical]["recipe"]["ORE"]
-
-        # if base_quantity % minimum_ore != 0:
-        #     base_quantity += minimum_quantity
-
-        total_ore += minimum_ore * (math.ceil(base_quantity / minimum_quantity))
-        print("Total Ore:", total_ore)
-    return total_ore
-
-
-def calculate_ore(chemical_details, reactions, ore):
-
-    produces_quantity = chemical_details["quantity"]
-    for chemical, quantity in chemical_details['recipe'].items():
-        print("Chemical:", chemical, "Quantity:", quantity)
-        nested_recipe = reactions[chemical]['recipe']
-        if 'ORE' in nested_recipe:
-            if chemical not in ore:
-                ore[chemical] = 0
-            ore[chemical] += quantity # * produces_quantity
-            print("ORE:", ore)
-        else:
-            calculate_ore(reactions[chemical], reactions, ore)
-    return ore
-
-
-def pseudo_code():
-    """
-    1 fuel = 7A + 1E
-    1 fuel = 7A + (7A + 1D)
-           = 14A + 1D
-           = 14A + (7A + 1C)
-           = 21A + 1C
-           = 21A + (7A + 1B)
-           = 28A +1B
-           = 28A + (1 ORE)
-           =
-
-    Note: dict where is just chemical, values are recipe and quantity
-    """
-    pass
+def perturb_pattern(pattern, repetition):
+    output_list = []
+    for digit in pattern:
+        for index in range(repetition):
+            output_list.append(digit)
+    return output_list
 
 
 def main():
-    # temporary input (make parsing function later)
-    raw_input = get_input()
-    reactions = parse_input(raw_input)
-    ore = calculate_total_ore(reactions)
-    print(f'{ore} ore required')
-
+    phase_input = get_input()
+    for phase in range(100):
+        phase_input = calculate_phase(phase_input)
+    return phase_input[:8]
 
 if __name__ == "__main__":
     print(main())
