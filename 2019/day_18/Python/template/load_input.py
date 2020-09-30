9f5762e472bdfd6ea6706008f4d400187c4b7aa5
Harold Dyson <hdyson@gmail.com>
2020-09-30 13:54:25 +0100

    Skeleton towards part 1

--- 2019/day_18/Python/template/load_input.py
+++ 2019/day_18/Python/template/load_input.py
@@ -4,7 +4,13 @@ from pathlib import Path
 def get_input():
     input_file = Path(__file__).resolve().parent.parent.parent / "input.txt"
     with open(input_file, "r") as file:
-        input_string = file.readline()
+        input_list = file.readlines()
 
-    input_list = [int(number) for number in input_string.strip()]
-    return input_list
+    lines = []
+    for line in input_list:
+        result = []
+        for character in line.strip():
+            result.append(character)
+        lines.append(result)
+
+    return lines
--- 2019/day_18/Python/template/part1.py
+++ 2019/day_18/Python/template/part1.py
@@ -1,8 +1,54 @@
 from load_input import get_input
 
+# Target: How many steps is the shortest path that collects all of the keys?
+#
+# Not all keys have doors
+#
+# Doors open as key is picked up
+#
+# Keys aren't in alphabetical order
+
+
+class Solver():
+    # available_directions depending on walls
+    # visible keys
+    # seen doors
+
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.current_position = None
+        self._find_at()
+
+    def look(self):
+        pass
+
+    def decide(self):
+        pass
+
+    def move(self):
+        pass
+
+    def _available_directions(self):
+        pass
+
+    def _find_at(self):
+        for row_index, row in enumerate(self.input_data):
+            for col_index, character in enumerate(row):
+                if character == '@':
+                    self.current_position = (row_index, col_index)
+                    return
+
+    def _find_visible_keys(self):
+        return []
+
+
+def count_steps(input_data):
+    return ""
+
 
 def main():
-    input_value = get_input()
+    input_data = get_input()
+    return count_steps(input_data)
 
 
 if __name__ == '__main__':
