cfc92b8d4c0ceb6bb364b2469339427f70c1d514
ChrisPaulBennett <christopher.bennett@metoffice.co.uk>
2020-10-30 14:20:12 +0000

    Initial commit of camel / banana puzzle

--- 2019/day_16/Python/template/load_input.py
+++ 2019/day_16/Python/template/load_input.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def get_input():
-    input_file = Path(__file__).resolve().parent.parent.parent / "input_personal.txt"
+    input_file = Path(__file__).resolve().parent.parent.parent / "input.txt"
     with open(input_file, "r") as file:
         input_string = file.readline()
 
