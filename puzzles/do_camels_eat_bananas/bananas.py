af7381be0bd2e76cf862dcb1a0b5b079983b6d0a
neal.carr <ncarr@vld585.cmpd1.metoffice.gov.uk>
2020-12-09 14:28:05 +0000

    Added some more methods to the Camel and began to build a round_trip World method

--- puzzles/do_camels_eat_bananas/bananas.py
+++ puzzles/do_camels_eat_bananas/bananas.py
@@ -7,7 +7,23 @@ class World:
         self.bananastores = {0: BananaStore(initial_bananas, initial_bananas)}
         self.step = step
         self.initial_bananas = initial_bananas
-
+        self.camel = None
+        
+    def spawn_camel(self, camel_carrying_capacity):
+        """
+        Create a camel in the world.
+        """
+        if self.camel is None:
+            self.camel = Camel(camel_carrying_capacity)
+        else:
+            raise AttributeError("There is only one camel.")
+    
+    def kill_camel(self):
+        """
+        >:)
+        """
+        self.camel = None
+    
     def get_final_bananas(self, camel_carrying_capacity):
         my_camel = Camel(camel_carrying_capacity)
         if my_camel.location in self.bananastores:
@@ -18,20 +34,53 @@ class World:
         # Camel needs to drop bananas, but keep enough to get back
         # Camel goes back to get move bananas
         return "Hello!"
+    
+    def round_trip(self, end_point):
+        """
+        Perform a round trip - update all banana stores and camel's banana store.
+        """
+        # Camel picks up bananas
+        bananas_at_start = self.bananastores[self.camel.location]
+        bananas_to_pick_up = max(self.camel.banana_store.remaining_capacity(), bananas_at_start.bananas)
+        self.camel.banana_store.add(bananas_to_pick_up)
+
+        # Camel walks to destination and drops bananas
+        distance = end_point - self.camel.location
+        self.camel.move(distance)
+        number_of_bananas_to_drop = self.camel.banana_store.bananas - self.camel.bananas_required(distance)
+
+
+
 
 
 class Camel:
 
-    def __init__(self, max_bananas):
-        self.max_bananas = max_bananas
-        self.banana_store = BananaStore(0, self.max_bananas)
+    def __init__(self, camel_carrying_capacity):
+        self.camel_carrying_capacity = camel_carrying_capacity
+        self.banana_consumption_rate = 1
+        self.banana_store = BananaStore(0, self.camel_carrying_capacity)
         self.location = 0
 
     def move(self, distance):
         """Positive distance moves towards destination
         negative distance moves towards the beginning"""
         self.location += distance
-        self.banana_store.remove(abs(distance))
+        self.banana_store.remove(self.bananas_required(distance))
+    
+    def bananas_required(self, distance):
+        return self.banana_consumption_rate * abs(distance)
+    
+    def possible_round_trip(self):
+        """
+        Return the maximum possible round trip distance, assuming the camel drops all its bananas.
+        """
+        return self.banana_store.bananas // 2
+    
+    def drop_bananas(self, box):
+        """
+
+        """
+    
 
 
 class BananaStore:
@@ -72,5 +121,6 @@ if __name__ == "__main__":
     for x in range(1000):
         step = x
         world = World(length, initial_bananas, step)
+        world.spawn_camel(camel_carrying_capacity)
         results = world.get_final_bananas(camel_carrying_capacity)
         print(results)
