6c021f6ff4f7b5016badb3a24e88ab50e579cda7
Harold Dyson <hdyson@gmail.com>
2021-03-23 12:59:02 +0000

    Part 1 complete

--- 2020/common/loaders.py
+++ 2020/common/loaders.py
@@ -1,6 +1,7 @@
 import os
 import __main__
 
+
 def load_string():
     """
     Returns each line from the input file
@@ -10,8 +11,7 @@ def load_string():
     : list of strings
         Each line from the index file
     """
-    filepath = os.path.join(os.path.dirname(__main__.__file__), "../input.txt") 
+    filepath = os.path.join(os.path.dirname(__main__.__file__), "../input.txt")
     with open(filepath) as file_handle:
         contents = file_handle.readlines()
     return [line.strip() for line in contents]
-
