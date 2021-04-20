0df82c1bfb5e92f25908fbc6440035a13f6729c6
R Sharp <roddy.sharp@metoffice.gov.uk>
2021-04-20 13:36:32 +0100

    Add initial code for day 4

---
+++ 2020/day_04/Python/passports.py
@@ -0,0 +1,58 @@
+from common.loaders import load_string
+
+class Passport:
+    def __init__(self, byr=None, iyr=None, eyr=None, hgt=None,
+                 hcl=None, ecl=None, pid=None, cid=None):
+        self.byr = byr
+        self.iyr = iyr
+        self.eyr = eyr
+        self.hgt = hgt
+        self.hcl = hcl
+        self.ecl = ecl
+        self.pid = pid
+        self.cid = cid
+
+    def validate(self):
+        '''Validate the entries in the passport :
+                1. that all required fields are present.
+                2. That no /extra/ fields have crept '''
+        pass
+
+    def __eq__(self, other):
+        '''test for equality in 2 passports'''
+        return all([self.byr == other.byr,
+                    self.iyr == other.iyr,
+                    self.eyr == other.eyr,
+                    self.hgt == other.hgt,
+                    self.hcl == other.hcl,
+                    self.ecl == other.ecl,
+                    self.pid == other.pid,
+                    self.cid == other.cid])
+        
+
+def parse_passport(lines):
+    '''Provided with lines from an input file, splits them into passport records
+    and then splits each record into the attributes of a passport'''
+    current_passport = Passport()
+    passports = []
+
+    for line in lines:
+        if line:
+            for entry in line.split():
+                key, value = entry.split(":")
+                setattr(current_passport, key, value)
+        else:
+            passports.append(current_passport)
+            current_passport = Passport()
+    return passports
+
+
+
+
+def main():
+    input_data = load_string()
+    something = parse_passport(input_data)
+    pass
+
+if __name__ == '__main__':
+    main()
