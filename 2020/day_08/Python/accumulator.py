64319ab7039cfbdda56845d4af45c29a96b8035f
Emma Hogan <emma.hogan@metoffice.gov.uk>
2021-08-24 13:39:12 +0100

    Add input for day 8, add instructions for day 8 part 2, add code and tests for day 8 part 2

--- 2020/day_08/Python/accumulator.py
+++ 2020/day_08/Python/accumulator.py
@@ -1,6 +1,8 @@
+from copy import copy
+
 from common.loaders import load_string
 
-def calculate_accumulator_value(program):
+def calculate_accumulator_value_part_1(program):
     """
     Return the accumulator value immediately before the instructions are
     repeated.
@@ -34,7 +36,72 @@ def calculate_accumulator_value(program):
     return accumulator_value
     
 
+def calculate_accumulator_value_part_2(program):
+    """
+    Return the accumulator value immediately before the instructions are
+    repeated.
+
+    Instructions are in the form ``XXX +/-#``:
+    
+
+    Parameters
+    ----------
+    program: list
+        The instructions in the program.
+
+    """
+    accumulator_value = 0
+    index = 0
+    seen_indices = set()
+    while index not in seen_indices:
+        seen_indices.add(index)
+        try:
+            instruction = program[index]
+        except IndexError:
+            if index == len(program):
+                return accumulator_value
+        operation, argument = instruction.split()
+        argument = int(argument)
+        if operation == "acc":
+            accumulator_value += argument
+            index += 1
+        elif operation == "jmp":
+            index += argument
+        elif operation == "nop":
+            index += 1
+        else:
+            raise RuntimeError(f"Invalid operation: {operation}")
+    raise RuntimeError("Infinite loop")
+
+
+def fix_program(program):
+    orig_program = program
+    accumulator_value = None
+    for index, instruction in enumerate(orig_program):
+        program = copy(orig_program)
+        if instruction.startswith("acc"):
+            continue
+        elif instruction.startswith("jmp"):
+            program[index] = program[index].replace("jmp", "nop")
+        elif instruction.startswith("nop"):
+            program[index] = program[index].replace("nop", "jmp")
+        else:
+            raise RuntimeError(f"Invalid instruction: {instruction}")
+        try:
+            accumulator_value = calculate_accumulator_value_part_2(program)
+        except RuntimeError:
+            pass
+        if accumulator_value:
+            print(f"Edited instruction at index {index}: "
+                  f"{orig_program[index]} --> {program[index]}")
+            break
+    return accumulator_value
+    
+
 if __name__ == "__main__":
     file_contents = load_string()
-    accumulator_value = calculate_accumulator_value(file_contents)
+    accumulator_value = calculate_accumulator_value_part_1(file_contents)
     print(f"Part 1: {accumulator_value}")
+    accumulator_value = fix_program(file_contents)
+    print(f"Part 2: {accumulator_value}")
+    
--- 2020/day_08/Python/test_accumulator.py
+++ 2020/day_08/Python/test_accumulator.py
@@ -1,6 +1,13 @@
-from accumulator import calculate_accumulator_value
+import pytest
 
-def test_accumulator_value():
+from accumulator import (
+    calculate_accumulator_value_part_1,
+    calculate_accumulator_value_part_2,
+    fix_program,
+)
+
+
+def test_accumulator_value_part_1():
     program = [
         "nop +0",
         "acc +1",
@@ -13,5 +20,55 @@ def test_accumulator_value():
         "acc +6",
     ]
     expected = 5
-    output = calculate_accumulator_value(program)
+    output = calculate_accumulator_value_part_1(program)
+    assert output == expected
+
+
+def test_accumlator_value_infinite_loop_part_2():
+    program = [
+        "jmp +0",
+        "acc +1",
+        "jmp +4",
+        "acc +3",
+        "jmp -3",
+        "acc -99",
+        "acc +1",
+        "jmp -4",
+        "acc +6",
+    ]
+    with pytest.raises(RuntimeError):
+        calculate_accumulator_value_part_2(program)
+
+
+def test_accumlator_value_program_terminates_part_2():
+    program = [
+        "nop +0",
+        "acc +1",
+        "jmp +4",
+        "acc +3",
+        "jmp -3",
+        "acc -99",
+        "acc +1",
+        "nop -4",
+        "acc +6",
+    ]
+    expected = 8
+    output = calculate_accumulator_value_part_2(program)
+    assert output == expected
+
+
+def test_fix_program():
+    program = [
+        "nop +0",
+        "acc +1",
+        "jmp +4",
+        "acc +3",
+        "jmp -3",
+        "acc -99",
+        "acc +1",
+        "jmp -4",
+        "acc +6",
+    ]
+    expected = 8        
+    output = fix_program(program)
     assert output == expected
