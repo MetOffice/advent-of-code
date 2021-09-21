0412ed3dd34b0b4bfe8fb3b81e7ca1260a82fa1d
Esther Turner <esther.turner@physics.ox.ac.uk>
2021-09-21 13:45:07 +0100

    Part 2 complete

--- 2020/day_09/Python/encoding.py
+++ 2020/day_09/Python/encoding.py
@@ -7,24 +7,47 @@ def format_input(file_contents):
     file_contents = [int(entry) for entry in file_contents]
     return file_contents
 
-def validate(file_contents, preamble_length=25):
+def find_invalid_number(file_contents, preamble_length=25):
     for index in range(preamble_length, len(file_contents)):
         buffer = file_contents[index - preamble_length : index]
-        if not check_combinations(file_contents[index], buffer):
+        if not validate_target(file_contents[index], buffer):
             return file_contents[index]
     raise RuntimeError('All values can be calculated from previous values')
 
 
-def check_combinations(target_value, combination_values):
+def validate_target(target_value, combination_values):
     potential_targets = combinations(combination_values, 2)
 
     for potential_target in potential_targets:
         if sum(potential_target) == target_value:
             return True
     return False
-    
+
+
+def find_chunk(values, target):
+    start_index = 0
+    end_index = 2
+
+    while sum(values[start_index : end_index]) != target:
+        total = sum(values[start_index : end_index])
+        if total < target:
+            end_index += 1
+        else:
+            start_index += 1
+        
+    return values[start_index : end_index]
+
+            
+def calculate_result(values):
+    return min(values) + max(values)
+
+
 if __name__ == "__main__":
     file_contents = load_string()
     input_list = format_input(file_contents)
-    print(validate(input_list))
+    target = find_invalid_number(input_list)
+
+    chunk = find_chunk(input_list, target)
+    result = calculate_result(chunk)
     
+    print(f'Part 1: Target {target}, Part 2: Result {result}')
--- 2020/day_09/Python/test_encoding.py
+++ 2020/day_09/Python/test_encoding.py
@@ -1,6 +1,6 @@
 import pytest
 
-from encoding import format_input, validate
+from encoding import format_input, find_invalid_number, find_chunk, calculate_result
 
 def test_format_input():
     input_values = ['1', '2', '3']
@@ -10,10 +10,25 @@ def test_format_input():
 
     assert expected == actual
 
-def test_validate():
+def test_find_invalid_number():
     int_list = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
     expected = 127
 
-    actual = validate(int_list, 5)
+    actual = find_invalid_number(int_list, 5)
+
+def test_find_chunk():
+    int_list = [35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576]
+    expected = [15, 25, 47, 40]
+    target = 127
+
+    actual = find_chunk(int_list, target)
+    
+    assert expected == actual
+
+def test_calculate_result():
+    int_list = [15, 25, 47, 40]    
+    expected = 62
+
+    actual = calculate_result(int_list)
 
     assert expected == actual
