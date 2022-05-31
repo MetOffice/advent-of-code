85b62a2671274b68ec78b401587f37b3bcc7e3a7
Emma Hogan <emma.hogan@metoffice.gov.uk>
2022-05-31 13:38:19 +0100

    Add solution for day 6 part 2

--- 2021/day_06/Python/test_fish.py
+++ 2021/day_06/Python/test_fish.py
@@ -1,17 +1,26 @@
-from fish_part1 import simulate_fish, calculate_total_fish
+import fish_part1
+import fish_part2
 
 
-def test_calculate_total_fish():
+def test_calculate_total_fish_part1():
     initial_state = [3, 4, 3, 1, 2]
     number_of_days = 80
     expected = 5934
-    actual = calculate_total_fish(initial_state, number_of_days)
+    actual = fish_part1.calculate_total_fish(initial_state, number_of_days)
     assert actual == expected
 
 
-def test_simulate_fish():
+def test_simulate_fish_part1():
     initial_state = [3, 4, 3, 1, 2]
     number_of_days = 5
     expected = [5, 6, 5, 3, 4, 5, 6, 7, 7, 8]
-    actual = simulate_fish(initial_state, number_of_days)
+    actual = fish_part1.simulate_fish(initial_state, number_of_days)
     assert actual == expected
+
+
+def test_calculate_total_fish_part2():
+    initial_state = [3, 4, 3, 1, 2]
+    number_of_days = 256
+    expected = 26984457539
+    actual = fish_part2.calculate_total_fish(initial_state, number_of_days)
+    assert actual == expected    
