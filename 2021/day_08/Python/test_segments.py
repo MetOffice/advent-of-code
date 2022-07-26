97c35c8f599d7acbb7cee1cba236927694c924c4
Harold Dyson <hdyson@gmail.com>
2022-07-26 13:07:05 +0100

    Added end to end test for part 2

--- 2021/day_08/Python/test_segments.py
+++ 2021/day_08/Python/test_segments.py
@@ -1,6 +1,4 @@
-import numpy as np
-
-from segments import main_part_1, count_unique_segments, parse_display_code
+from segments import main_part_1, main_part_2, count_unique_segments, parse_display_code
 
 
 def test_main_part_1():
@@ -36,3 +34,24 @@ def test_count_unique_segments():
     result_count = count_unique_segments(input_four_digits)
 
     assert result_count == 2
+
+
+def test_part2():
+    expected = 61229
+
+    display_codes = [
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+    ]
+
+    actual = main_part_2(display_codes)
+
+    assert actual == expected
