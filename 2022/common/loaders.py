fe16ba5891158354865d6d0570e42baae17e5962
Rhiannon Davies <26064197+rsdavies@users.noreply.github.com>
2023-01-17 13:35:30 +0000

    add the common files for this year

---
+++ 2022/common/loaders.py
@@ -0,0 +1,24 @@
+import os
+import __main__
+
+
+def load_string(filepath=None):
+    """
+    Returns each line from the input file
+
+    Returns
+    -------
+    : list of strings
+        Each line from the index file
+    """
+    if not filepath:
+        filepath = os.path.join(os.path.dirname(__main__.__file__), "../input.txt")
+
+    with open(filepath) as file_handle:
+        contents = file_handle.readlines()
+    return [line.strip() for line in contents]
+
+
+def load_ints():
+    strings = load_string()
+    return [int(string) if string else None for string in strings]
