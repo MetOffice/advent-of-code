251db060689c458483cfda34988cd13022a09e20
Rowan Baker <rowan.baker@metoffice.gov.uk>
2023-06-07 15:07:22 +0100

    Day 9, part 1. Wow, driving sucks. I am rehumbled by people who do the coding on-screen and don't immediately become dribbling morons.

---
+++ 2022/day_09/Python/ropebridge.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+class Knot:
+    def __init__(self) -> None:
+        self.position = np.array((0, 0))
+        self.follow_call = None
+
+    def update_position(self, direction_vector):
+        self.position += direction_vector
+        if self.follow_call:
+            self.follow_call()
+
+class Head(Knot):
+    direction = {
+        "U": np.array((0, 1)),
+        "R": np.array((1, 0)),
+        "D": np.array((0, -1)),
+        "L": np.array((-1, 0)),
+    }
+    def __init__(self) -> None:
+        super().__init__()
+
+    def wiggle_loop(self, instructions):
+        for instruction in instructions:
+            self.wiggle(instruction)
+
+    def wiggle(self, instruction):
+        self.update_position(Head.direction[instruction])
+        
+
+class Tail(Knot):
+    def __init__(self, head) -> None:
+        super().__init__()
+        self.head = head
+        head.follow_call = self.woggle
+        self.trail = set()
+
+    def woggle(self):
+        head_tail_vector = self.head.position - self.position 
+        if np.linalg.norm(head_tail_vector) >=2:
+            tail_move = np.clip(head_tail_vector, -1, 1)
+            self.update_position(tail_move)
+            self.trail.add(tuple(self.position))
+
+    def return_trail(self):
+        return len(self.trail)
+
+
+def read_instructions():
+    with open("../input.txt", "r") as input_instructions:
+        instructions = ""
+        for line in input_instructions:
+            direction, distance = line.strip().split()
+            instructions += (direction*int(distance))
+    return instructions
+
+def main():
+    instructions = read_instructions()
+    head = Head()
+    tail = Tail(head)
+    head.wiggle_loop(instructions)
+    print(tail.return_trail())
+
+if __name__=="__main__":
+    main()
