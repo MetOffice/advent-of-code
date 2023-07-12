07484b4aac065237e5bcc931462a79f10ebe4f36
Jamulus <james@lemonjam.me.uk>
2023-07-12 14:57:51 +0100

    A test-driven, object-oriented approach to day 10

---
+++ 2022/day_10/Python/test_crt.py
@@ -0,0 +1,178 @@
+import crt
+import pytest
+
+@pytest.mark.parametrize("cycle_number,x_value", [(0,  1),
+                                                  (1,  1),
+                                                  (2,  1),
+                                                  (3,  4),
+                                                  (4,  4),
+                                                  (5, -1)])
+def test_small_program(cycle_number, x_value):
+    small_program = \
+    """
+        noop
+        addx 3
+        addx -5
+    """
+
+    assert x_at_cycle(cycle_number) == x_value
+
+
+@pytest.mark.parametrize("cycle_number,x_value", [( 20, 21),
+                                                  ( 60, 19),
+                                                  (100, 18),
+                                                  (140, 21),
+                                                  (180, 16),
+                                                  (220, 18)])
+def test_large_program(cycle_number, x_value):
+    small_program = \
+    """
+        addx 15
+        addx -11
+        addx 6
+        addx -3
+        addx 5
+        addx -1
+        addx -8
+        addx 13
+        addx 4
+        noop
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx -35
+        addx 1
+        addx 24
+        addx -19
+        addx 1
+        addx 16
+        addx -11
+        noop
+        noop
+        addx 21
+        addx -15
+        noop
+        noop
+        addx -3
+        addx 9
+        addx 1
+        addx -3
+        addx 8
+        addx 1
+        addx 5
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx -36
+        noop
+        addx 1
+        addx 7
+        noop
+        noop
+        noop
+        addx 2
+        addx 6
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx 1
+        noop
+        noop
+        addx 7
+        addx 1
+        noop
+        addx -13
+        addx 13
+        addx 7
+        noop
+        addx 1
+        addx -33
+        noop
+        noop
+        noop
+        addx 2
+        noop
+        noop
+        noop
+        addx 8
+        noop
+        addx -1
+        addx 2
+        addx 1
+        noop
+        addx 17
+        addx -9
+        addx 1
+        addx 1
+        addx -3
+        addx 11
+        noop
+        noop
+        addx 1
+        noop
+        addx 1
+        noop
+        noop
+        addx -13
+        addx -19
+        addx 1
+        addx 3
+        addx 26
+        addx -30
+        addx 12
+        addx -1
+        addx 3
+        addx 1
+        noop
+        noop
+        noop
+        addx -9
+        addx 18
+        addx 1
+        addx 2
+        noop
+        noop
+        addx 9
+        noop
+        noop
+        noop
+        addx -1
+        addx 2
+        addx -37
+        addx 1
+        addx 3
+        noop
+        addx 15
+        addx -21
+        addx 22
+        addx -6
+        addx 1
+        noop
+        addx 2
+        addx 1
+        noop
+        addx -10
+        noop
+        noop
+        addx 20
+        addx 1
+        addx 2
+        addx 2
+        addx -6
+        addx -11
+        noop
+        noop
+        noop
+    """
+
+    assert x_at_cycle(cycle_number) == x_value
