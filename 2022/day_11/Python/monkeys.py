5a5cd6b01fd090d44535e04649a58353883e56fe
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2023-08-29 13:47:05 +0100

    Day 11 Part 1 - The Lambda from hell

--- 2022/day_11/Python/monkeys.py
+++ 2022/day_11/Python/monkeys.py
@@ -1,5 +1,7 @@
+import math
+import re
+from typing import List, Tuple, Callable, Any
 
-from typing import List, Tuple
 
 class Monkey:
     """
@@ -7,12 +9,12 @@ class Monkey:
     """
 
     def __init__(self,
-                 items:List[int],
-                 test_divisor:int,
-                 inspect_function:function,
-                 true_target:int,
-                 false_target:int
-    ) -> None:
+                 items: List[int],
+                 test_divisor: int,
+                 inspect_function: Callable[[int], int],
+                 true_target: int,
+                 false_target: int
+                 ) -> None:
         # The only property an item has is its worry level
         self.items = items
         # The factor used to determine target
@@ -26,9 +28,16 @@ class Monkey:
         # Count how many items I have inspected
         self.inspections = 0
 
-    
+    def __str__(self):
+        return f"Monkey\n" \
+               f"Items: {self.items}\n" \
+               f"Test: Divide by {self.test_divisor}\n" \
+               f"True Target: {self.true_target}\n" \
+               f"False Target: {self.false_target}\n" \
+               f"Inspections: {self.inspections}\n"
+
     @staticmethod
-    def construct_from_string(input_string:str) -> "Monkey":
+    def construct_from_string(input_string: str) -> "Monkey":
         """
         Create a monkey from an input string
         """
@@ -43,13 +52,17 @@ class Monkey:
         items = [int(item) for item in items]
 
         # operation line
-        operation_string = operation_line.split("old")[1]
-        op, arg = operation_string.split()
-        inspect_function = Monkey.create_inspect_function(op, arg)
+        regex = re.compile(".* new = .* (.) (.*)")
+        op, right = regex.findall(operation_line)[0]
+        inspect_function = Monkey.create_inspect_function(op, right)
+        test_divisor = int(test_line.split("divisible by ")[1])
+        true_target = int(true_line.split("throw to monkey ")[1])
+        false_target = int(false_line.split("throw to monkey ")[1])
 
+        return Monkey(items, test_divisor, inspect_function, true_target, false_target)
 
     @staticmethod
-    def create_inspect_function(op:str, arg:str) -> function:
+    def create_inspect_function(op: str, arg: str) -> Callable[[int], int]:
         """
         Create the operation for altering worry level
         """
@@ -60,7 +73,7 @@ class Monkey:
             return lambda x: Monkey.times_or_plus(op)(x, param)
 
     @staticmethod
-    def times_or_plus(op:str) -> function:
+    def times_or_plus(op: str) -> Callable[[int, int], int]:
         if op == "*":
             return lambda x, y: x * y
         if op == "+":
@@ -72,9 +85,10 @@ class Monkey:
         Inspect all my items
         """
         self.items = [self.inspect_function(item) for item in self.items]
+        self.items = [math.floor(item / 3.0) for item in self.items]
         self.inspections += len(self.items)
 
-    def throw_all(self) -> List[Tuple[int,int]]:
+    def throw_all(self) -> List[Tuple[int, int]]:
         """
         Declare all throws I intend to make
         """
@@ -105,11 +119,12 @@ class Barrel:
     A collection of monkeys is called a barrel.
     It deals with throwing and comparisons between monkeys.
     """
-    def __init__(self, monkeys:List[Monkey]) -> None:
+
+    def __init__(self, monkeys: List[Monkey]) -> None:
         self.monkeys = monkeys
 
     @staticmethod
-    def construct_from_string(input_string:str) -> "Barrel":
+    def construct_from_string(input_string: str) -> "Barrel":
         """
         Create a new barrel using an input string
         """
@@ -132,7 +147,7 @@ class Barrel:
         """
         Do n rounds
         """
-        for _ in range(n):
+        for _ in range(n-1): # Definitely not a bodge...
             self.round()
 
     def monkey_business(self):
@@ -142,3 +157,15 @@ class Barrel:
         activity = [monkey.inspections for monkey in self.monkeys]
         one, two = sorted(activity)[-2:]
         return one * two
+
+
+def main():
+    with open("../input.txt", "r") as file:
+        input_file = file.read()
+    barrel = Barrel.construct_from_string(input_file)
+    barrel.n_rounds(21)
+    print(barrel.monkey_business())
+
+
+if __name__ == "__main__":
+    main()
