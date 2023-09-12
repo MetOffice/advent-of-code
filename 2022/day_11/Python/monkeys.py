9bd21d593db9f9ea75267e904ce60a8a8ee7f520
Rhiannon Davies <26064197+rsdavies@users.noreply.github.com>
2023-09-12 13:51:16 +0100

    have improved performance, by a lot. But now have numerical drift with example solutions?

--- 2022/day_11/Python/monkeys.py
+++ 2022/day_11/Python/monkeys.py
@@ -1,6 +1,9 @@
 import math
+from cProfile import Profile
+from pstats import SortKey, Stats
 import re
 from typing import List, Tuple, Callable, Any
+import numpy as np
 
 
 class Monkey:
@@ -11,22 +14,24 @@ class Monkey:
     def __init__(self,
                  items: List[int],
                  test_divisor: int,
-                 inspect_function: Callable[[int], int],
                  true_target: int,
-                 false_target: int
+                 false_target: int,
+                 op, right
                  ) -> None:
         # The only property an item has is its worry level
-        self.items = items
+        self.items = np.asarray(items, dtype=np.int64)
         # The factor used to determine target
         self.test_divisor = test_divisor
         # The function used to adjust worry on inspection
-        self.inspect_function = inspect_function
+        # self.inspect_function = inspect_function
         # Target for if test is true
         self.true_target = true_target
         # Target for if test is false
         self.false_target = false_target
         # Count how many items I have inspected
         self.inspections = 0
+        self.op = op
+        self.right = right
 
     def __str__(self):
         return f"Monkey\n" \
@@ -54,38 +59,65 @@ class Monkey:
         # operation line
         regex = re.compile(".* new = .* (.) (.*)")
         op, right = regex.findall(operation_line)[0]
-        inspect_function = Monkey.create_inspect_function(op, right)
+        # inspect_function = Monkey.create_inspect_function(op, right)
         test_divisor = int(test_line.split("divisible by ")[1])
         true_target = int(true_line.split("throw to monkey ")[1])
         false_target = int(false_line.split("throw to monkey ")[1])
 
-        return Monkey(items, test_divisor, inspect_function, true_target, false_target)
+        return Monkey(items, test_divisor, true_target, false_target, op, right)
 
     @staticmethod
-    def create_inspect_function(op: str, arg: str) -> Callable[[int], int]:
-        """
-        Create the operation for altering worry level
-        """
-        if arg == "old":
-            return lambda x: Monkey.times_or_plus(op)(x, x)
-        else:
-            param = int(arg)
-            return lambda x: Monkey.times_or_plus(op)(x, param)
+    # def create_inspect_function(op: str, arg: str) -> Callable[[int], int]:
+    #     """
+    #     Create the operation for altering worry level
+    #     """
+    #     if arg == "old":
+    #         if op == "*":
+    #             return Monkey._self_times_self()
+    #     else:
+    #         param = int(arg)
+    #         return lambda x: Monkey.times_or_plus(op)(x, param)
 
+    # @staticmethod
+    # def times_or_plus(op: str) -> Callable[[int, int], int]:
+    #     if op == "*":
+    #         return lambda x, y: x * y
+    #     if op == "+":
+    #         return lambda x, y: x + y
+    #     raise Exception(f"{op} is not an operator")
     @staticmethod
-    def times_or_plus(op: str) -> Callable[[int, int], int]:
-        if op == "*":
-            return lambda x, y: x * y
-        if op == "+":
-            return lambda x, y: x + y
-        raise Exception(f"{op} is not an operator")
+    def _self_times_self(x):
+        return x * x
+    @staticmethod
+    def _self_times_other(x, y):
+        return x * y
+    @staticmethod
+    def _self_plus_self(x):
+        return x + x
+    @staticmethod
+    def _self_plus_other(x, y):
+        return x + y
 
     def inspect_all(self):
         """
         Inspect all my items
         """
-        self.items = [self.inspect_function(item) for item in self.items]
-        self.items = [math.floor(item / 3.0) for item in self.items]
+
+        if self.op == "*":
+            if self.right == "old":
+                self.items = self._self_times_self(self.items)
+            else:
+                self.items = self._self_times_other(self.items, int(self.right))
+
+        else:
+            if self.right == "old":
+                self.items = self._self_plus_self(self.items)
+            else:
+                self.items = self._self_plus_other(self.items, int(self.right))
+
+        # self.items = [self.inspect_function(item) for item in self.items]
+        # # self.items = [math.floor(item / 3.0) for item in self.items]
+        # self.items = np.floor(self.items/3.0)
         self.inspections += len(self.items)
 
     def throw_all(self) -> List[Tuple[int, int]]:
@@ -93,7 +125,7 @@ class Monkey:
         Declare all throws I intend to make
         """
         passes = [self.throw(item) for item in self.items]
-        self.items = []
+        self.items = np.asarray([], dtype=np.int64)
         return passes
 
     def throw(self, item):
@@ -111,7 +143,8 @@ class Monkey:
         """
         Receive an item into my inventory
         """
-        self.items.append(item)
+        self.items = np.append(self.items, item)
+
 
 
 class Barrel:
@@ -158,14 +191,21 @@ class Barrel:
         one, two = sorted(activity)[-2:]
         return one * two
 
+    def __repr__(self):
+        for monkey in self.monkeys:
+            print(monkey)
+        # TODO: fix
+        return "a"
 
 def main():
-    with open("../input.txt", "r") as file:
+    with open("../test_input.txt", "r") as file:
         input_file = file.read()
     barrel = Barrel.construct_from_string(input_file)
     barrel.n_rounds(20)
+    print(barrel)
     print(barrel.monkey_business())
 
 
 if __name__ == "__main__":
+    # TODO: weirdly have numerical drift after a large number of iterations?
     main()
