58ed29e333d8b213d9eaa9cfa621443bd94ba250
Jamulus <james@lemonjam.me.uk>
2023-12-05 12:53:56 +0000

    Day 13 part 2

--- 2022/day_13/Python/distress.py
+++ 2022/day_13/Python/distress.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import cmp_to_key
 
 def read_input(path="../input.txt"):
     """
@@ -35,7 +36,7 @@ def compare(packet_1, packet_2):
     # If both are integers simply compare them
     if isinstance(packet_1, int) and isinstance(packet_2, int):
         return np.sign(packet_1 - packet_2)
-    
+
     # If only one is an integer, wrap it in a one-element list
     if isinstance(packet_1, int):
         packet_1 = [packet_1]
@@ -47,7 +48,7 @@ def compare(packet_1, packet_2):
         out = compare(left, right)
         if out:
             return out
-    
+
     # If we run out of elements, compare the lengths of the lists
     return np.sign(len(packet_1) - len(packet_2))
 
@@ -68,8 +69,31 @@ def part_1(pairs):
             print(f"Warning! Pair at {i} is equal.")
     return total
 
+def part_2(pairs):
+    """
+    Given the list of pairs of inputs, sort the whole list and the two divier packets.
+    Find the indices of the divider packets in the final list and return their product.
+    """
+    # include dividers
+    packets = [[[2]], [[6]]]
+
+    # flatten list of packets
+    for pair in pairs:
+        packets += pair
+
+    # argsort. The indcies to which each item is sorted
+    packets = sorted(packets, key=cmp_to_key(compare))
+
+    index1 = packets.index([[2]])
+    index2 = packets.index([[6]])
+
+    return (index1 + 1) * (index2 + 1)
 
 if __name__ == "__main__":
     pairs = read_input()
+
     total = part_1(pairs)
     print(f"The total is {total}.")
+
+    decoder = part_2(pairs)
+    print(f"The decoder key is {decoder}.")
