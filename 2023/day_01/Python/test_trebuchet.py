016c73f241356af7c1d52913db8ac80c9f404557
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2024-01-23 13:08:35 +0000

    Day 1 Part 2

--- 2023/day_01/Python/test_trebuchet.py
+++ 2023/day_01/Python/test_trebuchet.py
@@ -1,4 +1,4 @@
-from trebuchet import find_first_n_last_digit
+from trebuchet import find_first_n_last_digit, main
 import pytest
  
 @pytest.mark.parametrize("input_string, result",
@@ -7,6 +7,22 @@ import pytest
     ("pqr3stu8vwx",38),
     ("a1b2c3d4e5f",15),
     ("treb7uchet",77),
+]
+)
+def test_find_first_n_last_digit(input_string, result):
+    '''This is a duck string'''
+    answer = find_first_n_last_digit(input_string)
+    assert answer == result
+
+@pytest.mark.parametrize("input_string, result",
+[
+    ("two1nine",29),
+    ("eightwothree",83),
+    ("abcone2threexyz",13),
+    ("xtwone3four",24),
+    ("4nineeightseven2",42),
+    ("zoneight234",14),
+    ("7pqrstsixteen",76),
 ]
 )
 def test_find_first_n_last_digit(input_string, result):
--- 2023/day_01/Python/trebuchet.py
+++ 2023/day_01/Python/trebuchet.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
-import re
-import sys
+import regex as re
 
-sys.path.extend(["/net/home/h05/frtz/PythonTraining/advent-of-code/2023"])
 from common.loaders import load_string
 
 
 def find_first_n_last_digit(input_string):
     ''' Uses regexes to find the first and last digits in a string, concattenate
     them and turn the result into an integer.'''
-    first_digit = re.search(r"^[a-zA-Z]*(\d).*$", input_string)
-    last_digit = re.search(r"^.*(\d)[a-zA-Z]*$", input_string)
-    number_string = first_digit.group(1) + last_digit.group(1)
-    return int(number_string)
+    new_reg = re.findall(r"one|two|three|four|five|six|seven|eight|nine|[1-9]", input_string, overlapped=True)
+    number_string = parse_number_string_to_int(new_reg[0])*10 + parse_number_string_to_int(new_reg[-1])
+    return number_string
+
+
+def parse_number_string_to_int(number_string):
+    '''Takes a string with a written number and return int for one to nine'''
+    num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6,
+                "seven": 7, "eight": 8, "nine": 9, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+                "9": 9}
+    return num_dict[number_string]
 
 
 def main():
     '''Main program to bolt all the bits n bobs together as well as handle admin
     such as loading the inputs and printing the result'''
     print("I am Groot")
-    inputs = load_string()
+    inputs = load_string("../input.txt")
     total = 0
     for input_string in inputs:
         total += find_first_n_last_digit(input_string)
 
-    print(f"Part 1: the total is {total}")
+    print(f"Part 2: the total is {total}")
+
 
 if __name__ == "__main__":
     main()
