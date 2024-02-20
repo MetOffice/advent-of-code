c68440f0907375b02bc8620bd7a8bc839a9fa581
Rowan Baker <rowan.baker@metoffice.gov.uk>
2024-02-20 13:39:46 +0000

    gondola, hard part just to come

--- 2023/day_03/Python/gondola.py
+++ 2023/day_03/Python/gondola.py
@@ -18,8 +18,18 @@ def find_parts(schematic_array):     # start with symbols and look for numbers
     list_of_things = []
     for i in range(row):
         for j in range(column):
-            if re.match(r"[^\d|\.]"):
+            if re.match(r"[^\d|\.]", schematic_array[i,j]):
                 list_of_things.append((i,j))
     return list_of_things
 
-def g(list_of_things):
+def find_part_numbers(list_of_things, schematic_array):
+    row, column  = schematic_array.shape
+    boundary_array = np.empty(row+2,column+2)
+    boundary_array[1:,1:] = schematic_array
+    for i,j in list_of_things:
+        i+=1
+        j+=1
+
+# Check if there's a number in the window we've built
+# and then regex back and forth until hitting a non-numerical character
+# and package those numbers found up as a part schematic number
