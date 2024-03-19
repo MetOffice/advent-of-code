5664fffe0754a0162f5ebeb368cef507d0cb46b9
Rhiannon Davies <26064197+rsdavies@users.noreply.github.com>
2024-03-19 13:31:50 +0000

    adds tests and passes part 1

--- 2023/day_03/Python/gondola.py
+++ 2023/day_03/Python/gondola.py
@@ -2,10 +2,7 @@ import re
 import numpy as np
 import sys
 
-sys.path.append("C:/Users/Rowan.baker/Advent-of-code/2023")
-
 from common.loaders import load_string
-
 
 def parse_input():
     # taking from loaders.py in common, turning our input into readable strings
@@ -28,32 +25,39 @@ def find_parts(schematic_array):  # start with symbols and look for numbers
     for i in range(row):
         for j in range(column):
             if re.match(r"[^\d|\.]", schematic_array[i, j]):
-                list_of_part_indicators.append((i, j))
+                list_of_part_indicators.append((j, i))
     return list_of_part_indicators
 
 
 def find_part_numbers(list_of_part_indicators, schematic_array):
     row, column = schematic_array.shape
     boundary_array = np.empty((row + 2, column + 2), dtype=str)
+    mask = np.zeros((row + 2, column + 2))
     boundary_array[1:-1, 1:-1] = schematic_array
-    part_number = ""
+
+    part_numbers = []
     for i, j in list_of_part_indicators:
+        # TODO: we need to be able to tell if one symbol has more than one number it matches
         i += 1
         j += 1
+        part_number = ""
         for mod_i in range(-1, 2):
             for mod_j in range(-1, 2):
-                if boundary_array[i + mod_i, j + mod_j].isnumeric():
+                if (boundary_array[j + mod_j, i + mod_i].isnumeric()) and (mask[j + mod_j, i + mod_i] == 0):
                     x_index = i + mod_i
-                    part_number = ""
-                    while boundary_array[x_index, j + mod_j].isnumeric():
-                        part_number = boundary_array[x_index, j + mod_j] + part_number
+                    while (boundary_array[j + mod_j, x_index].isnumeric()) and (mask[j + mod_j, x_index] == 0):
+                        part_number = boundary_array[j + mod_j, x_index] + part_number
+                        mask[j + mod_j, x_index] = 1
                         x_index -= 1
                     x_index = i + mod_i + 1
-                    while boundary_array[x_index, j + mod_j].isnumeric():
-                        part_number = part_number + boundary_array[x_index, j + mod_j]
+                    while (boundary_array[j + mod_j, x_index].isnumeric()) and (mask[j + mod_j, x_index] == 0):
+                        part_number = part_number + boundary_array[j + mod_j, x_index]
+                        mask[j + mod_j, x_index] = 1
                         x_index += 1
+                    part_numbers.append(part_number)
+                    part_number = ""
 
-            print(part_number)
+    return part_numbers
             # Parts number's borked. Gives us single digits, not multidigit numbers.
             # Also, we ought to write tests, I guess? Not enough testing up in this.
 
@@ -62,4 +66,8 @@ if __name__ == "__main__":
     input_schematic = parse_input()
     schematic_array = string_to_array(input_schematic)
     list_of_part_indicators = find_parts(schematic_array)
-    find_part_numbers(list_of_part_indicators, schematic_array)
+    part_numbers = find_part_numbers(list_of_part_indicators, schematic_array)
+    part_numbers = [int(number) for number in part_numbers]
+
+    result = sum(part_numbers)
+    print(result)
---
+++ 2023/day_03/Python/test_gondola.py
@@ -0,0 +1,35 @@
+import gondola
+import numpy as np
+
+test_input =['467..114..',
+             '...*......',
+             '..35..633.',
+             '......#...',
+             '617*......',
+             '.....+.58.',
+             '..592.....',
+             '......755.',
+             '...$.*....',
+             '.664.598..']
+
+test_array = np.array([list(word) for word in test_input])
+
+part_indicators = [(3, 1), (6, 3), (3, 4), (5, 5), (3, 8), (5, 8)]
+def test_string_to_array():
+    result = gondola.string_to_array(test_input)
+
+    assert np.array_equal(result, test_array)
+
+
+def test_find_parts():
+    parts = gondola.find_parts(test_array)
+
+    assert parts == part_indicators
+
+
+def test_find_part_numbers():
+    part_numbers = ['467', '35', '633', '617', '592', '755', '664', '598']
+
+    result = gondola.find_part_numbers(part_indicators, test_array)
+
+    assert set(result) == set(part_numbers)
