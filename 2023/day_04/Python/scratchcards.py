85e49d8369ce02f806def85dd92b81f441d24463
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2024-04-02 13:34:31 +0100

    Day 4 Part 2

--- 2023/day_04/Python/scratchcards.py
+++ 2023/day_04/Python/scratchcards.py
@@ -1,19 +1,33 @@
 from loaders import load_string
 
 
-def calculate_card_points(input_string):
+def number_of_matches(input_string: str):
     winning_numbers, chosen_numbers = input_string.split(": ")[1].split(" | ")
     winning_numbers = {int(x) for x in winning_numbers.split()}
     chosen_numbers = {int(x) for x in chosen_numbers.split()}
-    intersection = winning_numbers.intersection(chosen_numbers)
-    return int(2 ** (len(intersection) - 1))
+    return len(winning_numbers.intersection(chosen_numbers))
 
 
-def calculate_total_points(cards):
+def calculate_card_points(input_string: str):
+    return int(2 ** (number_of_matches(input_string) - 1))
+
+
+def calculate_total_points(cards: list[str]):
     return sum([calculate_card_points(card) for card in cards])
+
+
+def calculate_part2(cards: list[str]):
+    copies_of_cards = [1] * len(cards)
+    for i, card in enumerate(cards):
+        matches = number_of_matches(card)
+        for j in range(matches):
+            if i+j+1 > len(cards):
+                raise Exception("We have overrun the number of scratchcards!")
+            copies_of_cards[i+j+1] += copies_of_cards[i]
+    return sum(copies_of_cards)
 
 
 if __name__ == "__main__":
     scratchcards = load_string()
-    print(calculate_total_points(scratchcards))
-
+    print("Part 1: ", calculate_total_points(scratchcards))
+    print("Part 2: ", calculate_part2(scratchcards))
