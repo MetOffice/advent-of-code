c0ced5b9f8a1b184e28417161445b15c22ad3f92
Rhiannon Davies <26064197+rsdavies@users.noreply.github.com>
2024-04-30 13:12:47 +0100

    part one complete

--- 2023/day_05/Python/garden.py
+++ 2023/day_05/Python/garden.py
@@ -5,8 +5,8 @@ from loaders import load_string
 
 @dataclass
 class MapRange:
-    source_start: int
     destination_start: int
+    source_start: int
     range_length: int
 
     def destination_index(self, source_index: int) -> int | None:
@@ -34,20 +34,39 @@ class Map:
         return list(map(self.apply, source_indices))
 
 
-def parse_input(input_str: list[str]) -> tuple[list[int], list[Map]]:
-    input_iter = iter(input_str)
+    @staticmethod
+    def from_input(input_str: str) -> 'Map':
 
-    seeds = list(map(int, next(input_iter).split(':')[1].split()))
+        # the first line will make the name
+        first_line, *other_lines = input_str.split("\n")
+        source_name, destination_name = first_line.split(" ")[0].split("-to-")
+        ranges = []
+        for line in other_lines:
+            destination_start, source_start,  range_length = line.split(" ")
+            ranges.append(MapRange(int(destination_start), int(source_start), int(range_length)))
 
-    try:
-        while True:
-            line = next(input_iter)
-    except StopIteration:
-        pass
+        # the rest is all the MapRanges
+        return Map(source_name, destination_name, ranges)
 
-def get_lowest_location():
-    seeds, maps = parse_input(load_string())
 
+def parse_input(input_string: str) -> tuple[list[int], list[Map]]:
+
+    first_line, *everything_else = input_string.split('\n\n')
+
+    # handle first line to get the seeds
+    seeds = list(map(int, first_line.split(':')[1].split()))
+
+    # unpack the everything_else
+    the_maps = []
+    for thing in everything_else:
+        # make a Map
+        the_maps.append(Map.from_input(thing))
+
+
+    return seeds, the_maps
+
+
+def get_lowest_location(seeds, maps):
     map_dict = {index_map.source_name: index_map for index_map in maps}
 
     things = seeds
@@ -59,3 +78,13 @@ def get_lowest_location():
         current_thing = current_map.destination_name
 
     return min(things)
+
+
+if __name__ == "__main__":
+    input_data = "../input.txt"
+    with open(input_data) as file:
+        input_string = file.read()
+
+    seeds, maps = parse_input(input_string)
+    lowest = get_lowest_location(seeds, maps)
+    print(lowest)
