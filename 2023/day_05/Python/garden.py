d46df64aef2ded83b796007a013ec21ed4410a37
Adam Jacobs <adam.jacobs@metoffice.gov.uk>
2024-05-28 12:15:19 +0000

    Backwards seems less like the way to go

--- 2023/day_05/Python/garden.py
+++ 2023/day_05/Python/garden.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from loaders import load_string
 
@@ -14,6 +15,7 @@ class MapRange:
             return source_index - self.source_start + self.destination_start
 
         return None
+
 
 @dataclass
 class Map:
@@ -33,17 +35,18 @@ class Map:
     def apply_to_list(self, source_indices: list[int]) -> list[int]:
         return list(map(self.apply, source_indices))
 
-
     @staticmethod
-    def from_input(input_str: str) -> 'Map':
+    def from_input(input_str: str) -> "Map":
 
         # the first line will make the name
         first_line, *other_lines = input_str.split("\n")
         source_name, destination_name = first_line.split(" ")[0].split("-to-")
         ranges = []
         for line in other_lines:
-            destination_start, source_start,  range_length = line.split(" ")
-            ranges.append(MapRange(int(destination_start), int(source_start), int(range_length)))
+            destination_start, source_start, range_length = line.split(" ")
+            ranges.append(
+                MapRange(int(destination_start), int(source_start), int(range_length))
+            )
 
         # the rest is all the MapRanges
         return Map(source_name, destination_name, ranges)
@@ -51,17 +54,16 @@ class Map:
 
 def parse_input(input_string: str) -> tuple[list[int], list[Map]]:
 
-    first_line, *everything_else = input_string.split('\n\n')
+    first_line, *everything_else = input_string.split("\n\n")
 
     # handle first line to get the seeds
-    seeds = list(map(int, first_line.split(':')[1].split()))
+    seeds = list(map(int, first_line.split(":")[1].split()))
 
     # unpack the everything_else
     the_maps = []
     for thing in everything_else:
         # make a Map
         the_maps.append(Map.from_input(thing))
-
 
     return seeds, the_maps
 
@@ -80,10 +82,13 @@ def get_lowest_location(seeds, maps):
     return min(things)
 
 
-def get_lowest_seed(seeds, maps, within_limits=None):
-    seed_ranges = sorted(list(zip(seeds[::2], seeds[1::2])))
-
+def get_lowest_seed(
+    seed_ranges: list[tuple[int, int]],
+    maps: list[Map],
+    within_limits: Optional[tuple[int, int]] = None,
+):
     if not maps:
+        assert within_limits
         for seed_range in seed_ranges:
             top = seed_range[0] + seed_range[1]
             bottom = seed_range[0]
@@ -99,12 +104,60 @@ def get_lowest_seed(seeds, maps, within_limits=None):
 
         return None
 
+    else:
+        current_range_start = within_limits[0] if within_limits else 0
+
+        # First range that goes past the start
+        source_ranges = iter(sorted(maps[-1].ranges, key=lambda r: r.source_start))
+        next_source_range = next(source_ranges)
+        while next_source_range.source_start + next_source_range.range_length <= current_range_start:
+            next_source_range = next(source_ranges)
+        destination_ranges = iter(
+            sorted(maps[-1].ranges, key=lambda r: r.destination_start)
+        )
+        next_dest_range = next(destination_ranges)
+        while next_dest_range.destination_start + next_dest_range.range_length <= current_range_start:
+            next_dest_range = next(destination_ranges)
+
+        # Current if it includes the start
+        current_source_range = (
+            next_source_range
+            if next_source_range.source_start <= current_range_start
+            and (
+                next_source_range.source_start + next_source_range.range_length
+                > current_range_start
+            )
+            else None
+        )
+        if current_source_range:
+            next_source_range = next(source_ranges)
+        current_dest_range = (
+            next_dest_range
+            if next_dest_range.destination_start <= current_range_start
+            and (
+                next_dest_range.destination_start + next_dest_range.range_length
+                > current_range_start
+            )
+            else None
+        )
+        if current_dest_range:
+            next_dest_range = next(destination_ranges)
+
+        while within_limits is None or current_range_start < within_limits[1]:
+            if current_dest_range:
+                result = get_lowest_seed(seed_ranges, maps[:-1], (current_range_start, current_dest_range.destination_start + current_dest_range.range_length))
+                pass
+
     # trying depth first search first before the paragraph below
     # - choose lowest range (including not-MapRanges) within limits
     # - compute what sub ranges the next map will split those into,
     #   doing the map backwards
     # - recurse depth first starting with the lowest range, and
     #   then increasing if a seed is not found
+
+    # 28/05/2024
+    # Multiple ranges could go to the same place, including unmapped ranges
+    # You know what, maybe do it ↓ that way ↓ after all
 
     # seeds is a collection of ranges now...
     # (seednum, range), (seednum, range) -> (start1, end1), (start2, end2)
@@ -124,3 +177,5 @@ if __name__ == "__main__":
     seeds, maps = parse_input(input_string)
     lowest = get_lowest_location(seeds, maps)
     print(lowest)
+
+    seed_ranges = sorted(list(zip(seeds[::2], seeds[1::2])))
