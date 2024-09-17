541cbae2f426027e5cf298687873e3d36b21bdb7
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2024-09-17 12:49:26 +0100

    Day 8, finally!

--- 2023/day_08/Python/haunted_wasteland.py
+++ 2023/day_08/Python/haunted_wasteland.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import multiprocessing as mp
 import os
+from datetime import datetime
 
 test_data = """RL
 
@@ -105,28 +106,40 @@ if __name__ == "__main__":
     instruction_index = 0
     full_count = 0
 
-    current_nodes = {node for node in nodes_dict if node[-1] == "A"}
+    current_nodes = [node for node in nodes_dict if node[-1] == "A"]
 
-    loop_detection: tuple[list[LocationOfZ], ...] = tuple([] for node in current_nodes)
+    #loop_detection: tuple[list[LocationOfZ], ...] = tuple([] for node in current_nodes)
 
-    # While any ghost is at a node not ending in Z
-    while any(current_node[-1] != "Z" for current_node in current_nodes):
+    start_time = datetime.now()
+    with open("output2.csv", "a") as f:
+        # While any ghost is at a node not ending in Z
+        while any(current_node[-1] != "Z" for current_node in current_nodes):
 
-        # Step each ghost forward according to the current instruction direction
-        current_nodes = {
-            nodes_dict[current_node][instructions[instruction_index]]
-            for current_node in current_nodes
-        }
-        instruction_index += 1
-        full_count += 1
-        if instruction_index > len(instructions) - 1:
-            instruction_index = 0
+            # Step each ghost forward according to the current instruction direction
+            current_nodes = [
+                nodes_dict[current_node][instructions[instruction_index]]
+                for current_node in current_nodes
+            ]
 
-        cases = [current_node[-1] != "Z" for current_node in current_nodes]
-        if not any(cases):
-            print(full_count)
-            print(current_nodes)
-            exit()
-        # NOTE for next time: Adam said something, do that
+            cases = [current_node[-1] == "Z" for current_node in current_nodes]
+            if any(cases):
+                for i, case in enumerate(cases):
+                    if case:
+                        print(f"{i}, {current_nodes[i]}, {instruction_index}, {full_count}", file=f)
 
-    print(full_count)
+            instruction_index += 1
+            full_count += 1
+            if instruction_index > len(instructions) - 1:
+                instruction_index = 0
+            if full_count >= 1000:
+                break
+
+        print(full_count)
+    end_time = datetime.now()
+    print(end_time - start_time)
+    # This would have taken 3700 years to complete.
+    # Instead, we wrote out any time the ghost reached a Z node.
+    # Putting this in excel, each ghost looped on a regular interval.
+    # The lowest common multiple of these loops is the answer
+    # math.lcm(15871,21251,16409,11567,18023,14257)
+
