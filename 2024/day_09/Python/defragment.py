1cf08b85677a58d41b2f67deb368a4ec8dc21f2b
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2025-05-13 14:25:16 +0100

    Day 9 almost done

--- 2024/day_09/Python/defragment.py
+++ 2024/day_09/Python/defragment.py
@@ -11,7 +11,7 @@ def load_input(filename: str) -> str:
     return [line.strip() for line in lines][0]
 
 
-def rearrange_inplace(inp: list[str]) -> None:
+def rearrange_inplace_part1(inp: list[str]) -> None:
     first_free_space = next_free_space(inp, 0)
     last_char = next_char_backwards(inp, len(inp) - 1)
 
@@ -22,11 +22,71 @@ def rearrange_inplace(inp: list[str]) -> None:
         last_char = next_char_backwards(inp, last_char)
 
 
+def scan_for_space(inp, size_of_file, max):
+    next_free = next_free_space(inp, 0)
+
+    while (size_of_space := get_size_of_space(inp, next_free)) < size_of_file:
+        next_free = next_free_space(inp, next_free + size_of_space)
+        if next_free > max:
+            raise IndexError
+    return next_free
+
+
+def rearrange_inplace_part2(inp: list[str]) -> None:
+    last_visited = 0
+    last_char = next_char_backwards(inp, len(inp) - 1)
+
+    processed_ids = set()
+
+    while True:
+
+        print(inp[last_char])
+        size_of_file = get_size_of_file(inp, last_char)
+        if inp[last_char] in processed_ids:
+            print(f"Already seen {inp[last_char]}")
+            last_char = next_char_backwards(inp, last_char - size_of_file)
+            continue
+        if last_char - size_of_file == 0:
+            print("end")
+            break
+        processed_ids.add(inp[last_char])
+        try:
+            index_of_space = scan_for_space(inp, size_of_file, last_char)
+        except IndexError:
+            print(f"Could not find space for file {inp[last_char]} with size {size_of_file}")
+        else:
+            place_in_position(inp, inp[last_char], index_of_space, size_of_file)
+            place_in_position(inp, '.', last_char - size_of_file + 1, size_of_file)
+            last_visited = index_of_space
+        last_char = next_char_backwards(inp, last_char - size_of_file)
+
+
+def place_in_position(inp: list[int | str], chr: int | str, start: int, length: int):
+    for i in range(start, start + length):
+        inp[i] = chr
+
 
 def next_free_space(inp: list[str], current_index) -> int:
     while inp[current_index] != ".":
         current_index += 1
     return current_index
+
+
+def get_size_of_space(inp: list[str | int], start_index):
+    index = start_index
+    while inp[index] == ".":
+        index += 1
+        if index >= len(inp):
+            raise IndexError
+    return index - start_index
+
+
+def get_size_of_file(inp: list[str | int], start_index):
+    index = start_index
+    chr = inp[start_index]
+    while inp[index] == chr and index > 0:
+        index -= 1
+    return start_index - index
 
 
 def next_char_backwards(inp: list[str], current_index) -> int:
@@ -55,14 +115,18 @@ def expand_input(inp: list[str]) -> list[str]:
 
     return result
 
+
 def checksum(inp: list[str]) -> int:
-    return sum(i*j for i,j in enumerate(inp) if j != '.')
+    return sum(i * j for i, j in enumerate(inp) if j != '.')
+
 
 def main():
     input_line = load_input("input")
+    files = input_line[::2]
+    space = input_line[1::2]
 
     expanded = expand_input(input_line)
-    rearrange_inplace(expanded)
+    rearrange_inplace_part2(expanded)
     result = checksum(expanded)
     print(result)
 
