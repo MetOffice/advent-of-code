ba00e2a868fdc10396cfd851f4ed7d6ea697b545
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2025-09-30 14:13:43 +0100

    Day 15 And then a step to the right

--- 2024/day_15/Python/boxes.py
+++ 2024/day_15/Python/boxes.py
@@ -1,3 +1,4 @@
+import itertools
 from dataclasses import dataclass
 import numpy as np
 
@@ -19,4 +20,39 @@ def read_input(filename: str) -> Input:
     )
 
 
-print(read_input("test_input.txt"))
+def find_robot(warehouse: np.ndarray):
+    res = np.where(warehouse == '@')
+    return res[0][0], res[1][0]
+
+def step_right(warehouse: np.ndarray, robot):
+    """
+    Operates in-place!!!!!!!
+    """
+    # Get the line to the right of the robot
+    line = warehouse[robot[0]][robot[1]:]
+
+    # Stop at the first obstacle or empty space
+    line_of_interest = np.array(list(itertools.takewhile(lambda c: c not in '.#', line)))
+
+    # Character that stops the movement
+    stop_character = line[len(line_of_interest)]
+
+    # Cannot move
+    if stop_character == '#':
+        return
+
+    # Re-insert the original line back into the warehouse offset by 1
+    warehouse[robot[0], robot[1] + 1: robot[1] + len(line_of_interest) + 1] = line_of_interest
+    # Remove the original position of the robot
+    warehouse[robot[0], robot[1]] = '.'
+
+def main():
+    inp = read_input("test_input.txt")
+    robot = find_robot(inp.warehouse)
+    step_right(inp.warehouse, robot)
+
+    print(inp.warehouse)
+
+
+if __name__ == "__main__":
+    main()
