f01c86b8151a1f1eed8eb34e50a37382a503d7b2
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2025-11-11 13:58:20 +0000

    Day 16 Pt1 - Some Types

--- 2024/day_16/Python/maze.py
+++ 2024/day_16/Python/maze.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+from typing import NamedTuple, Any, Optional
 
 import numpy as np
 
@@ -24,38 +25,46 @@ def find_end(maze: np.ndarray):
     return end_pos[0][0], end_pos[1][0]
 
 
-def calculate_costs(maze, thingy):
-    current_cost, point, direction = thingy
+class Item(NamedTuple):
+    cost: int
+    position: tuple[int, int]
+    direction: tuple[int, int]
+    parent: Optional["Item"]
 
-    forward = (current_cost + 1, (point[0] + direction[0], point[1] + direction[1]), direction)
-    left = (current_cost + 1001, (point[0] - direction[1], point[1] + direction[0]), (-direction[1], direction[0]))
-    right = (current_cost + 1001, (point[0] + direction[1], point[1] - direction[0]), (direction[1], -direction[0]))
 
-    valid_moves = [x for x in [forward, left, right] if maze[x[1]] in '.E']
+def calculate_costs(maze, thingy: Item) -> list[Item]:
+    current_cost, point, direction, parent = thingy
+
+    forward = Item(current_cost + 1, (point[0] + direction[0], point[1] + direction[1]), direction, thingy)
+    left = Item(current_cost + 1001, (point[0] - direction[1], point[1] + direction[0]), (-direction[1], direction[0]),
+                thingy)
+    right = Item(current_cost + 1001, (point[0] + direction[1], point[1] - direction[0]), (direction[1], -direction[0]),
+                 thingy)
+
+    valid_moves = [x for x in [forward, left, right] if maze[x.position] in '.E']
 
     return valid_moves
 
 
-def bfs(maze: np.ndarray, start: tuple, end: tuple):
+def bfs(maze: np.ndarray, start: tuple[int, int]):
     start_direction = (0, 1)
     visited = set()
-    queue = PriorityQueue()
-    queue.put((0, start, start_direction))
+    queue: PriorityQueue[Item] = PriorityQueue()
+    queue.put(Item(0, start, start_direction, None))
 
     while True:
         nex = queue.get()
-        visited.add(nex[1])
-        if maze[nex[1]] == "E":
-            return nex[0]
-        thing = calculate_costs(maze, nex)
-        [queue.put(t) for t in thing if t[1] not in visited]
+        visited.add(nex.position)
+        if maze[nex.position] == "E":
+            return nex.cost
+        things = calculate_costs(maze, nex)
+        [queue.put(t) for t in things if t.position not in visited]
 
 
 def main():
     maze = read_input("input")
     start = find_start(maze)
-    end = find_end(maze)
-    result = bfs(maze, start, end)
+    result = bfs(maze, start)
     print(result)
 
 
