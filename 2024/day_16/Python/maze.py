2d10f0e120f1b2d2b075314f4b199662ffdad433
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2025-12-23 12:35:07 +0000

    Revert "Day 16 Pt2 Attempt"
    This reverts commit 11906b838ab021a9d5c8b3b0cf15a98608532c53.

--- 2024/day_16/Python/maze.py
+++ 2024/day_16/Python/maze.py
@@ -29,67 +29,43 @@ class Item(NamedTuple):
     cost: int
     position: tuple[int, int]
     direction: tuple[int, int]
-    parents: list["Item"]
+    parent: Optional["Item"]
 
 
-def calculate_valid_moves_and_costs(maze, thingy: Item) -> list[Item]:
+def calculate_costs(maze, thingy: Item) -> list[Item]:
     current_cost, point, direction, parent = thingy
 
-    forward = Item(current_cost + 1, (point[0] + direction[0], point[1] + direction[1]), direction, [thingy])
+    forward = Item(current_cost + 1, (point[0] + direction[0], point[1] + direction[1]), direction, thingy)
     left = Item(current_cost + 1001, (point[0] - direction[1], point[1] + direction[0]), (-direction[1], direction[0]),
-                [thingy])
+                thingy)
     right = Item(current_cost + 1001, (point[0] + direction[1], point[1] - direction[0]), (direction[1], -direction[0]),
-                 [thingy])
+                 thingy)
 
     valid_moves = [x for x in [forward, left, right] if maze[x.position] in '.E']
 
     return valid_moves
 
 
-
 def bfs(maze: np.ndarray, start: tuple[int, int]):
     start_direction = (0, 1)
-    visited_2 : dict[tuple[int, int], Item] = {}
+    visited = set()
     queue: PriorityQueue[Item] = PriorityQueue()
     queue.put(Item(0, start, start_direction, None))
 
     while True:
-        current_item = queue.get()
-        visited_2[current_item.position] = current_item
-        if maze[current_item.position] == "E":
-            return current_item
-        moves = calculate_valid_moves_and_costs(maze, current_item)
-        for move in moves:
-            assert move.cost >= current_item.cost
-            if move.position not in visited_2.keys():
-                queue.put(move)
-            elif abs(visited_2.get(move.position).cost - move.cost) in [1000,0,1]:
-                print(f"SPLIT!{visited_2.get(move.position).cost}=?{move.cost},{move.position}")
-                f = visited_2[move.position].parents
-                f.append(current_item)
-            else:
-                print(f"     {visited_2.get(move.position).cost}=?{move.cost},{move.position}")
-
-def traceback(item: Item) -> list[Item]:
-    if item.parents is None:
-        return [item]
-    r = []
-    r.extend(item.parents)
-    print(len(item.parents))
-    for i in item.parents:
-        t = traceback(i)
-        r.extend(t)
-    return r
+        nex = queue.get()
+        visited.add(nex.position)
+        if maze[nex.position] == "E":
+            return nex.cost
+        things = calculate_costs(maze, nex)
+        [queue.put(t) for t in things if t.position not in visited]
 
 
 def main():
     maze = read_input("input")
     start = find_start(maze)
     result = bfs(maze, start)
-
-    visited = set(i.position for i in traceback(result))
-
-    print(len(visited))
+    print(result)
 
 
 if __name__ == "__main__":
