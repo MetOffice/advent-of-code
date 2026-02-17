52d65b64bcfdecabf2e7cc4db2d5a3caf2428fb2
Phil Relton <phil.relton@metoffice.gov.uk>
2026-02-17 13:00:44 +0000

    Adding superb super fast version of day 2 that worked with no tests needed. Well done Phil

--- 2025/day_02/Python/invalid_ids.py
+++ 2025/day_02/Python/invalid_ids.py
@@ -1,5 +1,5 @@
-import itertools
-from math import ceil
+from tqdm import tqdm
+from math import ceil, floor
 
 
 def read_file(file_path) -> list[list[str]]:
@@ -8,30 +8,43 @@ def read_file(file_path) -> list[list[str]]:
     return [rang.split("-") for rang in line.split(",")]
 
 
-def invalid_in_range(rang: list[str]) -> list[str]:
-    start, end = rang
-    half_length = ceil(len(start) / 2) # 4
-    start_half = start[:half_length]
+def string_mirror_check(number: int) -> bool:
+    string_number = str(number)
+    Nchar = len(string_number)
+    if Nchar % 2:
+        # It's odd
+        return False
+    first, second = string_number[:Nchar//2], string_number[Nchar//2:]
+    if first == second:
+        return True
 
-    poissible_numbers = generate_ids_of_length(half_length, int(start_half))
-    invalid = [i for i in poissible_numbers if int(start) <= i <= int(end)]
-    return invalid
 
-
-def generate_ids_of_length(half_length, start_number: int):
-    numbers = []
-    for i in range(start_number, int("9"*half_length)):
-        actual_number = i + i*(10**half_length)
-        numbers.append(actual_number)
-    return numbers
-
+def string_chunk_check(number: int) -> bool:
+    string_number = str(number)
+    SN_length = len(string_number)
+    longest = floor(len(string_number)//2)
+    for length in range(1, longest+1):
+        if not SN_length % length:
+            if string_number[:length] * (SN_length//length) == string_number:
+                return True
+    else:
+        return False
 
 
 def main():
     data = read_file("../input.txt")
-    all_invalid = list(itertools.chain.from_iterable(invalid_in_range(a) for a in data))
-    result = sum(all_invalid)
-    print(result)
+    invalid_ids = []
+    invalid_ids2 = []
+    for thing in tqdm(data, total=len(data)):
+        for thing2 in range(int(thing[0]), int(thing[1])+1):
+            if string_mirror_check(thing2):
+                invalid_ids.append(thing2)
+            if string_chunk_check(thing2):
+                invalid_ids2.append(thing2)
+    result = sum(invalid_ids)
+    print("Part 1 answer:", result)
+    result2 = sum(invalid_ids2)
+    print("Part 2 answer:", result2)
 
 
 if __name__ == "__main__":
