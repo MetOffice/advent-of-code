e030ffe50afe2a565d3437ec0c16b92e1d13d003
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-03-17 12:40:05 +0000

    day 4 part 2

--- 2025/day_04/Python/toilet_paper.py
+++ 2025/day_04/Python/toilet_paper.py
@@ -14,21 +14,36 @@ def load_input(file_path) -> ndarray[tuple[Any, ...], dtype[Any]]:
     array[array == "."] = 0
     return array.astype(int)
 
+def one_step(data):
+    pass
+
+box = np.array(
+    [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+)
+
 def main():
     input_data = load_input('../input.txt')
     # Then convolve it!
-    neighbours = ndimage.convolve(input_data.astype(int), np.array(
-        [
-            [1, 1, 1],
-            [1, 1, 1],
-            [1, 1, 1]
-        ]
-    ), mode="constant", cval=0)
-    # Mask by locations with paper rolls
-    masked = np.multiply(neighbours, input_data)
-    masked[np.logical_and(1 < masked, masked < 5)] = 1
-    masked[masked >= 5] = 0
-    result = sum(sum(masked))
+    result = 0
+    while True:
+        neighbours = ndimage.convolve(input_data.astype(int), box, mode="constant", cval=0)
+        # Mask by locations with paper rolls
+        masked = np.multiply(neighbours, input_data)
+        masked[np.logical_and(1 < masked, masked < 5)] = 1
+        masked[masked >= 5] = 0
+        removed = sum(sum(masked))
+
+        inverted = np.logical_not(masked).astype(int)
+        input_data = np.multiply(inverted, input_data)
+
+        if removed == 0:
+            break
+        result += removed
+
     print(result)
 
 if __name__ == "__main__":
