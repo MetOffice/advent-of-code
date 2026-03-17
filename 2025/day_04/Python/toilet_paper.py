1f5ee3d51da30c91c1bb12b0036fb2e3f90d5bf6
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-03-17 12:27:12 +0000

    day 4 part 1

--- 2025/day_04/Python/toilet_paper.py
+++ 2025/day_04/Python/toilet_paper.py
@@ -2,25 +2,34 @@ from typing import Any
 
 import numpy as np
 from numpy import dtype, ndarray
+from scipy import ndimage
 
 
 def load_input(file_path) -> ndarray[tuple[Any, ...], dtype[Any]]:
     with open(file_path, 'r') as file:
         lines = file.readlines()
     lines = list([list(line.strip()) for line in lines])
-    return np.array(lines)
-
-def convert_to_int(array: ndarray):
+    array = np.array(lines)
     array[array == "@"] = 1
     array[array == "."] = 0
-
+    return array.astype(int)
 
 def main():
     input_data = load_input('../input.txt')
-    convert_to_int(input_data)
-    input_data.astype(int)
-    print(input_data)
     # Then convolve it!
+    neighbours = ndimage.convolve(input_data.astype(int), np.array(
+        [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1]
+        ]
+    ), mode="constant", cval=0)
+    # Mask by locations with paper rolls
+    masked = np.multiply(neighbours, input_data)
+    masked[np.logical_and(1 < masked, masked < 5)] = 1
+    masked[masked >= 5] = 0
+    result = sum(sum(masked))
+    print(result)
 
 if __name__ == "__main__":
     main()
