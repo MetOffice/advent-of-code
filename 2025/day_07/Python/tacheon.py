a20fa9f14e998efb36be05f6535a96f6705c922a
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-03-31 13:23:06 +0100

    day 7 part 1

---
+++ 2025/day_07/Python/tacheon.py
@@ -0,0 +1,40 @@
+import numpy as np
+import tqdm
+
+
+def load_input(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    lines = list([list(line.strip()) for line in lines])
+    return np.array(lines)
+
+
+def iterate_once(row: int, array):
+    beams = np.where(array[row] == "|")
+    split_count = 0
+    for beam in list(*beams):
+        if array[row+1][beam] == "^":
+            split_count += 1
+            array[row+1, beam-1] = "|"
+            array[row+1, beam+1] = "|"
+        else:
+            array[row+1, beam] = "|"
+    return split_count
+
+
+
+def main():
+    t = load_input("../input.txt")
+    start = np.where(t == "S")
+    t[(start[0] + 1, start[1])] = "|"
+    iterate_once(1, t)
+
+    total = 0
+    for i in tqdm.tqdm(range(1, len(t)-1)):
+        total += iterate_once(i, t)
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
