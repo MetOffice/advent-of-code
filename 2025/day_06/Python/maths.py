2def7d669ddcfd7bdf3465c16ce7ce7608fa981f
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-03-31 12:43:55 +0100

    day 6 part 2

--- 2025/day_06/Python/maths.py
+++ 2025/day_06/Python/maths.py
@@ -1,3 +1,4 @@
+import re
 from functools import reduce
 from operator import mul, add
 
@@ -9,20 +10,45 @@ def load_input(file_path):
     return lines
 
 
+def load_input_2(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    lines = list([list(line) for line in lines])
+    return lines
+
+
 ops = {
     "+": add,
     "*": mul
 }
 
 
-def main():
-    input_data = load_input('../input.txt')
+def part_1(input_data):
     sums = list(zip(*input_data))
     total = 0
     for sum in sums:
         total += reduce(ops[sum[-1]], map(int, sum[:-1]))
 
-    print(total)
+    print("Part1:", total)
+
+
+def part_2(input_data):
+    operators = input_data[-1]
+    indexes = [i for i, o in enumerate(operators) if o in ["+", "*"]]
+    total = 0
+    for start, end in list(zip(indexes, indexes[1:] + [len(input_data[0])])):
+        operator = ops[operators[start]]
+        numbers = [x[start:end] for x in input_data]
+        parsed_numbers = [int("".join(x[col] for x in numbers[:-1])) for col in range(len(numbers[0]) - 1)]
+        total += reduce(operator, parsed_numbers)
+    print("Part2:", total)
+    return total
+
+
+def main():
+    input_data = load_input('../input.txt')
+    part_1(input_data)
+    part_2(load_input_2('../input.txt'))
 
 
 if __name__ == "__main__":
