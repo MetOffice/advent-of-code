45443a0c95f5b7fdcb541cf7590be347d4d3a869
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-04-10 11:25:10 +0100

    day 7: animation

--- 2025/day_07/Python/tacheon.py
+++ 2025/day_07/Python/tacheon.py
@@ -1,5 +1,6 @@
+from time import sleep
+
 import numpy as np
-import tqdm
 
 
 def load_input(file_path):
@@ -21,18 +22,29 @@ def iterate_once(row: int, array):
             array[row+1, beam] = "|"
     return split_count
 
-
+frames = []
+def write_array_as_frame(array):
+    frame = "\n".join("".join(row) for row in array)
+    frames.append(frame)
+    print("\033[0;0H") # Reset cursor to top left
+    print(frame)
 
 def main():
     t = load_input("../input.txt")
     start = np.where(t == "S")
+    write_array_as_frame(t)
     t[(start[0] + 1, start[1])] = "|"
+    write_array_as_frame(t)
 
     total = 0
-    for i in tqdm.trange(1, len(t)-1):
+    for i in range(1, len(t)-1):
         total += iterate_once(i, t)
+        write_array_as_frame(t)
+        sleep(0.05) # For animation
+
 
     print(total)
+
 
 
 if __name__ == "__main__":
