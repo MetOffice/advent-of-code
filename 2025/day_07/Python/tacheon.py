5fe2172fda0ec0bc0d33f2802f5a59a0533738bd
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-04-28 13:03:26 +0100

    day 7: part 2

--- 2025/day_07/Python/tacheon.py
+++ 2025/day_07/Python/tacheon.py
@@ -7,19 +7,21 @@ def load_input(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     lines = list([list(line.strip()) for line in lines])
-    return np.array(lines)
+    # This should be an int array...
+    return np.array(lines, dtype=(str, 15))
 
 
 def iterate_once(row: int, array):
-    beams = np.where(array[row] == "|")
+    beams = np.where(np.char.isdigit(array[row]))
     split_count = 0
     for beam in list(*beams):
+        beam_val = int(array[row, beam])
         if array[row+1][beam] == "^":
             split_count += 1
-            array[row+1, beam-1] = "|"
-            array[row+1, beam+1] = "|"
+            array[row+1, beam-1] = int(array[row+1, beam-1].replace(".","0")) + beam_val
+            array[row+1, beam+1] = int(array[row+1, beam+1].replace(".","0")) + beam_val
         else:
-            array[row+1, beam] = "|"
+            array[row+1, beam] = int(array[row+1, beam].replace(".","0")) + beam_val
     return split_count
 
 frames = []
@@ -29,21 +31,31 @@ def write_array_as_frame(array):
     print("\033[0;0H") # Reset cursor to top left
     print(frame)
 
+
+def count_last_row(t):
+    last_row = t[-1]
+    ints = [ int(a) for a in last_row if a.isdigit() ]
+    return sum(ints)
+
+
 def main():
     t = load_input("../input.txt")
     start = np.where(t == "S")
     write_array_as_frame(t)
-    t[(start[0] + 1, start[1])] = "|"
+    t[(start[0] + 1, start[1])] = "1"
     write_array_as_frame(t)
 
     total = 0
     for i in range(1, len(t)-1):
         total += iterate_once(i, t)
         write_array_as_frame(t)
-        sleep(0.05) # For animation
+        # sleep(1) # For animation
+
+    result2 = count_last_row(t)
 
 
-    print(total)
+    print("Pt1:" + str(total))
+    print("Pt2:" + str(result2))
 
 
 
