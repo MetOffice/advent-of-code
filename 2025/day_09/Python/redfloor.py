5dc3f6cfb5c12282fd92ab51120b28359733df6a
Philip Lyes <32490263+philip-ly@users.noreply.github.com>
2026-06-09 13:28:13 +0100

    day 9: pt 2

--- 2025/day_09/Python/redfloor.py
+++ 2025/day_09/Python/redfloor.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tqdm
+from shapely import Polygon
 
 
 def read_file():
@@ -20,7 +22,12 @@ def calc_pairs(array):
             result.append((calc_area(array[i], array[j]), i, j))
     return result
 
-def main():
+def get_bounds(red_tiles):
+    max_x = max(t[0] for t in red_tiles)
+    max_y = max(t[1] for t in red_tiles)
+    return max_x, max_y
+
+def part1():
     red_tiles = read_file()
     pairs = calc_pairs(red_tiles)
     pairs_sorted = sorted(pairs, key=lambda tup: tup[0], reverse=True)
@@ -29,7 +36,28 @@ def main():
     print(red_tiles[biggest[2]])
     print(biggest)
 
+def part2():
+    red_tiles = read_file()
+    pairs = calc_pairs(red_tiles)
+    pairs_sorted = sorted(pairs, key=lambda tup: tup[0], reverse=True)
+
+    tiles = Polygon(red_tiles)
+
+    for pair in tqdm.tqdm(pairs_sorted):
+        pt1 = red_tiles[pair[1]]
+        pt2 = red_tiles[pair[2]]
+        square = Polygon([
+            (pt1[0], pt1[1]),
+            (pt2[0], pt1[1]),
+            (pt2[0], pt2[1]),
+            (pt1[0], pt2[1])
+        ])
+        if tiles.covers(square):
+            print(pair)
+            return
+    print()
+
 
 
 if __name__ == "__main__":
-    main()
+    part2()
